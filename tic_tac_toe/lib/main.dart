a1d7efa96b496694375420b492b57a40520fb91d
SebastianSkowron <sebastian.skowron.dev@gmail.com>
2023-03-06 18:27:57 +0100

    added draw pop-up and winner pop_up, working on scoreboard

--- tic_tac_toe/lib/main.dart
+++ tic_tac_toe/lib/main.dart
@@ -63,6 +63,81 @@ class _HomePageState extends State<HomePage> {
         _board[index] = _currentPlayer;
         _currentPlayer = _currentPlayer == 'X' ? 'O' : 'X';
       });
+      String winner = _checkWinner();
+      if (winner != '') {
+        _showWinnerDialog(winner);
+      } else if (_board.every((element) => element != '')) {
+        _showDrawDialog();
+      }
     }
+  }
+
+  String _checkWinner() {
+    List<List<int>> lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (var line in lines) {
+      if (_board[line[0]] != '' &&
+          _board[line[0]] == _board[line[1]] &&
+          _board[line[1]] == _board[line[2]]) {
+        return _board[line[0]];
+      }
+    }
+    return '';
+  }
+
+  void _resetBoard() {
+    setState(() {
+      _board = List.filled(9, '');
+      _currentPlayer = 'X';
+    });
+    Navigator.of(context).pop();
+  }
+
+  void _showWinnerDialog(String winner) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Winner'),
+          content: Text('$winner has won!'),
+          actions: <Widget>[
+            TextButton(
+              child: Text('Play again'),
+              onPressed: () {
+                _resetBoard();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void _showDrawDialog() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Draw'),
+          content: Text('There are no more empty spaces.'),
+          actions: <Widget>[
+            TextButton(
+              child: Text('Play again'),
+              onPressed: () {
+                _resetBoard();
+              },
+            ),
+          ],
+        );
+      },
+    );
   }
 }
