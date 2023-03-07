d07858a1de2d7ca3ca4075d84e0674ab580ce07f
SebastianSkowron <sebastian.skowron.dev@gmail.com>
2023-03-07 23:36:57 +0100

    changing the game_screen

--- tic_tac_toe/lib/screens/game_screen.dart
+++ tic_tac_toe/lib/screens/game_screen.dart
@@ -1,63 +1,42 @@
 import 'package:flutter/material.dart';
-import 'package:tic_tac_toe/utils/constants.dart';
-import 'package:tic_tac_toe/logic/game_logic.dart';
+import 'package:tic_tac_toe/models/game.dart';
 
 class GameScreen extends StatefulWidget {
-  final GameLogic gameLogic;
-
-  const GameScreen({required this.gameLogic})
-
   @override
   _GameScreenState createState() => _GameScreenState();
 }
 
 class _GameScreenState extends State<GameScreen> {
-  GameLogic gameLogic = GameLogic();
+  Game game = Game();
 
-    void _onSquareTapped(int row, int col) {
+  void makeMove(int row, int col) {
     setState(() {
-      widget.gameLogic.playMove(row, col);
+      game.makeMove(row, col);
+      if (game.isOver()) {
+        showGameOverPopup();
+      }
     });
+  }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Tic Tac Toe'),
-      ),
-      body: Center(
-        child: Container(
-          padding: EdgeInsets.all(kBoardPadding),
-          decoration: BoxDecoration(
-            border: Border.all(
-              color: kBorderColor,
-              width: kBorderWidth,
-            ),
-          ),
-          child: AspectRatio(
-            aspectRatio: 1,
-            child: GridView.builder(
-              physics: const NeverScrollableScrollPhysics(),
-              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: kBoardSize.toInt(),
-              ),
-              itemBuilder: (context, index) {
-                int row = (index / kBoardSize).floor();
-                int col = (index % kBoardSize).floor();
-                return GestureDetector(
-                  onTap: () => _onSquareTapped(row, col),
-                  child: Container(
-                  width: kSquareSize,
-                  height: kSquareSize,
-                  color: getSquareColor(row, col),
-                  child: getSymbol(gameLogic.board[row][col]),
-                ),
-              );
+  void showGameOverPopup() {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: Text('Game Over'),
+        content: game.winner == null
+            ? Text('Draw')
+            : Text('${game.winner} wins!'),
+        actions: [
+          ElevatedButton(
+            child: Text('Play Again'),
+            onPressed: () {
+              Navigator.pop(context);
+              setState(() {
+                game.reset();
+              });
             },
           ),
-        ),
+        ],
       ),
-    )
     );
-}
-}}
+  }
